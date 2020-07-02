6cccfd48d155cf7d90dc013137d742fb0239e86d
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-07-02 14:52:41 +1000

    Created initial files and testing CI

---
+++ kalman_filter.h
@@ -0,0 +1,22 @@
+// Kalman Filter
+// The Kalman Filter is used for approximating variables by feeding in information from other
+// that are directly correlation as some type of equation.
+
+// Kalman Filter follows the equations:
+// Predict Stage
+// x_k = F(x_(k-1)) + Bu            - Predict Future State
+// P = F(P_(k-1))(F^Transpose) + Q  - Predict Future Covariance
+
+// Update Stage
+// S = HP(H_T) + R                  - Estimate System Uncertainty (Associated measurement noise)
+// K = PH/S                         - Calculate Kalman Gain
+// y = z - H(x_k)                   - Calculate Residual (Difference between predicted and measurement values)
+// x_final = (x_k) + Ky             - Update State
+// P_final = (I-KH)P                - Update Covariance
+
+#include <stdint.h>
+
+#pragma once
+
+class kalman_filter {
+};
