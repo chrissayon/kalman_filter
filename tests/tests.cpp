7cd26f74e5af3d8651eec2fc496b17eb1bc028ce
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-07-03 18:55:01 +1000

    Added kalman filter on tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,23 +4,17 @@
 #define CATCH_CONFIG_MAIN
 // #define CATCH_CONFIG_ALL_PARTS
 #include "catch.hpp"
+#include "kalman_filter.h"
 
 TEST_CASE("Pass Tests")
 {
-    REQUIRE(1 == 1);
+    kalman_filter kalmanFilter;
+    kalmanFilter.predict(1, 10);
+    // kalmanFilter.covariance_matrix
+
+    REQUIRE(8.25 == kalmanFilter.covariance_matrix(0,0));
+    REQUIRE(0.25 == kalmanFilter.process_noise_matrix(0,0));
 }
 
-
-// Compile & run:
-// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -o 010-TestCase 010-TestCase.cpp && 010-TestCase --success
-// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% 010-TestCase.cpp && 010-TestCase --success
-
 // Expected compact output (all assertions):
-//
-// prompt> 010-TestCase --reporter compact --success
-// 010-TestCase.cpp:14: failed: Factorial(0) == 1 for: 0 == 1
-// 010-TestCase.cpp:18: passed: Factorial(1) == 1 for: 1 == 1
-// 010-TestCase.cpp:19: passed: Factorial(2) == 2 for: 2 == 2
-// 010-TestCase.cpp:20: passed: Factorial(3) == 6 for: 6 == 6
-// 010-TestCase.cpp:21: passed: Factorial(10) == 3628800 for: 3628800 (0x375f00) == 3628800 (0x375f00)
-// Failed 1 test case, failed 1 assertion.
+// prompt> *.exe --reporter compact --success
